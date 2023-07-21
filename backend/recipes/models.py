b635e84068971d358ef6673648c30bc3d2bc15a9
Sergey Sukhartsev <bloxazavr@mail.ru>
2023-07-21 09:11:50 +0300

    описана модель Корзина Покупок

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -109,3 +109,30 @@ class FavoriteRecipe(models.Model):
 
     def __str__(self):
         return f"{self.user} добавил {self.recipe.name} в избраннное"
+
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь"
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name="Рецепт"
+    )
+
+    class Meta:
+        ordering = ("-id")
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "recipe"],
+                name="unique_user_shopping_cart"
+            )
+        ]
+        verbose_name = "Список покупок"
+        verbose_name_plural = "Списки покупок'"
+
+    def __str__(self):
+        return f"{self.user} добавил {self.recipe.name} в список покупок"
