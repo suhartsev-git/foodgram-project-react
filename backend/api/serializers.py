5bcd21356c5ae267f8014b1459eb817fad68c67c
Sergey Sukhartsev <bloxazavr@mail.ru>
2023-07-21 09:32:48 +0300

    прописал вью и сериализатор Tag

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,7 +2,7 @@ from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 
 from users.models import User, Subscription
-
+from recipes.models import Tag
 
 class UserCreateSerializerCustom(UserCreateSerializer):
 
@@ -26,3 +26,11 @@ class UserSerializerCustom(UserSerializer):
 
     model = User
     lookup_field = 'username'
+
+
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = "__all__"
+
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,16 @@
-rom djoser.views import UserViewSet
+from djoser.views import UserViewSet
+from rest_framework import viewsets
 
 from users.models import Subscription, User
-from api.serializers import UserSerializerCustom
+from api.serializers import UserSerializerCustom, TagSerializer
+from recipes.models import Tag
+
 
 class UserViewSetCustom(UserViewSet):
     queryset = User.objects.all()
+    pass
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    serializer_class = TagSerializer
+    queryset = Tag.objects.all()
