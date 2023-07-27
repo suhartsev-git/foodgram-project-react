4b8f9338cebc145188508848446a445f272aa393
Sergey Sukhartsev <bloxazavr@mail.ru>
2023-07-27 19:21:09 +0300

    the first review

--- backend/recipes/apps.py
+++ backend/recipes/apps.py
@@ -4,7 +4,8 @@ from django.apps import AppConfig
 class RecipesConfig(AppConfig):
     """
     Конфигурация приложения 'recipes'.
-    Класс 'RecipesConfig' используется для настройки приложения 'recipes' в Django.
+    Класс 'RecipesConfig' используется
+    для настройки приложения 'recipes' в Django.
     Он определяет параметры конфигурации для данного приложения.
     """
     default_auto_field = 'django.db.models.BigAutoField'
--- backend/users/apps.py
+++ backend/users/apps.py
@@ -4,7 +4,8 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     """
     Конфигурация приложения 'users'.
-    Класс 'UsersConfig' используется для настройки приложения 'users' в Django.
+    Класс 'UsersConfig' используется
+    для настройки приложения 'users' в Django.
     Он определяет параметры конфигурации для данного приложения.
     """
     default_auto_field = 'django.db.models.BigAutoField'
